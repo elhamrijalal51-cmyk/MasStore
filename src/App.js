7d525d19182484fbfdcb0dd988bf8f23b9ea6d32
Jalal Elhamri <elhamrijalal51@gmail.com>
2025-10-21 14:23:09 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="app" >
+      <h1>Salam Jalal 👋</h1>
+      <p>Bienvenue dans ton premier composant React.</p>
+      <div style={{ backgroundColor: '#d1ecf1', padding: '10px', marginTop: '10px' }}>
+        <h2>Ma première div imbriquée</h2>
+        <p>Voici du contenu à l’intérieur.</p>
+        <input type="text"/> <br/>
+        <img src="logoMas.png"/>
+      </div>
+    </div>
+  );
+}
+export default App;
